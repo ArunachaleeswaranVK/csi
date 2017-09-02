aec02d4788bc3173aa8dd2a584ba6d1fd26c4fbd
Arun VK <arun_bolt4@yahoo.in>
2017-09-02 16:43:22 +0000

    filled all the main tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'inst.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Institute.new
+  t.imno = row['IM No']
+  t.name = row['Instititution Name']
+  t.city = row['City']
+  t.district = row['District']
+  t.state = row['State']
+  t.pincode = row['Pincode']
+  t.nominating_authority = row['Authority']
+  t.na_phone = row['Phone']
+  t.na_email = row['Email']
+  t.join_date = row['JoinDt']
+  t.valid_till = row['Valid-Date']
+  t.validity = row['Validity']
+  t.save
+  puts "#{t.imno}, #{t.name} saved"
+end
+
+puts "There are now #{Institute.count} rows in the transactions table"
