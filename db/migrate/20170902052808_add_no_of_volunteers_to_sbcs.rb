b355b95a4f2d45e4637352aced137d4feb916141
Arun VK <arun_bolt4@yahoo.in>
2017-09-04 22:39:50 +0000

    commit

--- db/migrate/20170902052808_add_no_of_volunteers_to_sbcs.rb
+++ db/migrate/20170902052808_add_no_of_volunteers_to_sbcs.rb
@@ -1,5 +1,5 @@
 class AddNoOfVolunteersToSbcs < ActiveRecord::Migration
   def change
-    add_column :sbcs , :no_of_volunteers , :date
+    add_column :sbcs , :no_of_volunteers , :integer
   end
 end
--- db/migrate/20170902052902_change_no_of_volunteers_to_sbcs.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNoOfVolunteersToSbcs < ActiveRecord::Migration
-  def change
-     change_column :sbcs , :no_of_volunteers , :integer
-  end
-end
