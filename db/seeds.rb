b7b1d5cc5fd5ea5ed47608e214d5b1e5cee9ab90
Arun VK <arun_bolt4@yahoo.in>
2017-09-13 14:54:09 +0000

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,13 @@ require 'csv'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'chapter.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  t = Chapter.new
-  t.chapter_name = row['Chapter Name']
-  t.region = row['Region']
-  t.category = row['Category']
-  t.name = row['Name']
-  t.designation = row['Designation']
+  t = User.new
+ 
   t.email = row['Email']
-  t.mobile = row['Mobile']
+  t.password = row['Mobile']
 
   t.save
-  puts "#{t.chapter_name}, #{t.name} saved"
+  puts "#{t.email}, #{t.password} saved"
 end
 
-puts "There are now #{Chapter} rows in the transactions table"
+puts "There are now #{User} rows in the transactions table"
