f6fc1add7a19ee798bd2197abe6dbcb9353f462b
Arun VK <arun_bolt4@yahoo.in>
2017-09-20 15:33:20 +0000

    commit

--- db/migrate/20170920144855_remove_institute_from_bestthesis.rb
+++ db/migrate/20170920144855_remove_institute_from_bestthesis.rb
@@ -1,5 +1,5 @@
 class RemoveInstituteFromBestthesis < ActiveRecord::Migration
   def change
-     remove_column :bestthesisawards , :imno
+     remove_column :bestthesisawards , :imno , :string
   end
 end
